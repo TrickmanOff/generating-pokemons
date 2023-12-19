488e59c58416868a89a8ec2733e04143e2fafcf2
TrickmanOff <egorkadv2002@gmail.com>
2023-12-19 21:37:56 +0300

    add fixed models

--- lib/discriminators.py
+++ lib/discriminators.py
@@ -71,3 +71,49 @@ class DCDiscriminator(Discriminator):
         else:
             x = self.final_act(x)
         return x
+
+
+class FixedDCDiscriminator(Discriminator):
+    LRELU_SLOPE = 0.2
+
+    def __init__(self, latent_channels: int = 1024, image_channels: int = 3):
+        super().__init__()
+
+        self.convs = nn.ModuleList([
+            # (B, image_channels, 64, 64)
+            nn.Conv2d(image_channels, latent_channels // 8, kernel_size=4, stride=2, padding=1,
+                      bias=False),  # (B, latent_channels // 8, 32, 32)
+            nn.Conv2d(latent_channels // 8, latent_channels // 4, kernel_size=4, stride=2,
+                      padding=1, bias=False),  # (B, latent_channels // 4, 16, 16)
+            nn.Conv2d(latent_channels // 4, latent_channels // 2, kernel_size=4, stride=2,
+                      padding=1, bias=False),  # (B, latent_channels // 2, 8, 8)
+            nn.Conv2d(latent_channels // 2, latent_channels, kernel_size=4, stride=2, padding=1,
+                      bias=False),  # (B, latent_channels, 4, 4)
+            nn.Conv2d(latent_channels, 1, kernel_size=4,
+                      bias=False),  # (B, 1, 1, 1)
+        ])
+
+        self.post_convs = nn.ModuleList(
+            [nn.LeakyReLU(self.LRELU_SLOPE)] +
+            [
+                nn.Sequential(nn.BatchNorm2d(conv.out_channels), nn.LeakyReLU(self.LRELU_SLOPE))
+                for conv in self.convs[1:-1]
+            ]
+        )
+
+        self.final_act = nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor, y=None) -> torch.Tensor:
+        """
+        :param x: of shape (B, image_channels, 64, 64)
+        :returns: of shape (B, 1)
+        """
+        if y is not None:
+            raise RuntimeError('Condition not supported')
+
+        for conv, post_conv in zip(self.convs[:-1], self.post_convs):
+            x = post_conv(conv(x))
+        x = self.convs[-1](x)  # (B, 1, 1, 1)
+        x = x.flatten(1)  # (B, 1)
+        x = self.final_act(x)
+        return x
